b341a202f409ba6556fb172496e2d479c1b23d73
catatsuy <catatsuy@catatsuy.org>
2021-06-06 15:31:44 +0900

    add image_builder

---
+++ webapp/golang/image_builder/image.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	db *sqlx.DB
+)
+
+const (
+	postsPerPage  = 20
+	ISO8601Format = "2006-01-02T15:04:05-07:00"
+	UploadLimit   = 10 * 1024 * 1024 // 10mb
+)
+
+type User struct {
+	ID          int       `db:"id"`
+	AccountName string    `db:"account_name"`
+	Passhash    string    `db:"passhash"`
+	Authority   int       `db:"authority"`
+	DelFlg      int       `db:"del_flg"`
+	CreatedAt   time.Time `db:"created_at"`
+}
+
+type Post struct {
+	ID           int       `db:"id"`
+	UserID       int       `db:"user_id"`
+	Imgdata      []byte    `db:"imgdata"`
+	Body         string    `db:"body"`
+	Mime         string    `db:"mime"`
+	CreatedAt    time.Time `db:"created_at"`
+	CommentCount int
+	Comments     []Comment
+	User         User
+	CSRFToken    string
+}
+
+type Comment struct {
+	ID        int       `db:"id"`
+	PostID    int       `db:"post_id"`
+	UserID    int       `db:"user_id"`
+	Comment   string    `db:"comment"`
+	CreatedAt time.Time `db:"created_at"`
+	User      User
+}
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+	host := os.Getenv("ISUCONP_DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("ISUCONP_DB_PORT")
+	if port == "" {
+		port = "3306"
+	}
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatalf("Failed to read DB port number from an environment variable ISUCONP_DB_PORT.\nError: %s", err.Error())
+	}
+	user := os.Getenv("ISUCONP_DB_USER")
+	if user == "" {
+		user = "root"
+	}
+	password := os.Getenv("ISUCONP_DB_PASSWORD")
+	dbname := os.Getenv("ISUCONP_DB_NAME")
+	if dbname == "" {
+		dbname = "isuconp"
+	}
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local&interpolateParams=true",
+		user,
+		password,
+		host,
+		port,
+		dbname,
+	)
+
+	db, err = sqlx.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %s.", err.Error())
+	}
+	defer db.Close()
+
+	posts := make([]Post, 0, 100)
+
+	for offset := 0; ; offset += 100 {
+		err := db.Select(&posts, "SELECT * FROM `posts` ORDER BY `id` ASC LIMIT 100 OFFSET ?", offset)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(posts) == 0 {
+			break
+		}
+
+		for _, p := range posts {
+			err = writeImage(p)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func writeImage(post Post) error {
+	ext := ""
+	switch post.Mime {
+	case "image/jpeg":
+		ext = "jpg"
+	case "image/png":
+		ext = "png"
+	case "image/gif":
+		ext = "gif"
+	}
+
+	err := os.WriteFile(fmt.Sprintf("%d.%s", post.ID, ext), post.Imgdata, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
